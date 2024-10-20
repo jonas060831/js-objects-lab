10151b5fd3747c23f7afb7b3ca89d25a48595165
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-10-20 03:01:04 -0700

    answer on exercise 12

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ Also, log the `game.items` array to confirm that the pokeball quantity is being 
 Solve Exercise 11 here:
 */
 
-
+//copy catch pokemon decrease the pokeball quantity
 game.catchPokemon = function(pokemonObj) {
 
     //add the pokemonObj to the game.party array
@@ -209,14 +209,28 @@ game.catchPokemon = function(pokemonObj) {
     //reduce pokeball
     game.items.forEach( item => {
 
+        //its ok the have a negative pokeball
         if(item.name === 'pokeball') item.quantity--
     })
 }
 
 const kangaskhan = pokemon[114]
-
+//after the update catch a pokemon
 game.catchPokemon(kangaskhan)
-
+//then log the game items
 console.log(game.items)
 
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+
+game.gyms.forEach( gym => {
+
+    if(gym.difficulty < 6) gym.completed = true
+})
 
