2a2e11f1d2a4b957a2c6d5d0a19628f9a14da5f8
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-10-20 04:09:13 -0700

    answer on exercise 16

--- app.js
+++ app.js
@@ -303,3 +303,14 @@ game.gyms.forEach( gym => {
 })
 
 
+/*
+Exercise 16
+1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
+
+
+Solve Exercise 16 here:
+*/
+
+
+
+console.log(game)
